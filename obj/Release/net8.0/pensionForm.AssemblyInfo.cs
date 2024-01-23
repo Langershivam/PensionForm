a0d517eb304c4f91d06df5c68ed5ee4053202c50
Langershivam <shivamlanger2001@gmail.com>
2024-01-23 12:37:57 +0530

    netlify

--- obj/Release/net8.0/pensionForm.AssemblyInfo.cs
+++ obj/Release/net8.0/pensionForm.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("pensionForm")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42c52b3c9e8213b830e9b1bb02f2acadee2565ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("pensionForm")]
